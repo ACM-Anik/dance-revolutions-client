2fc4d9f185853ca4c709b84c71239defa8658604
ACM-Anik <anikmojumder5252@gmail.com>
2023-06-12 10:07:58 +0600

    small changes in Authprovider

--- src/Providers/AuthProvider.jsx
+++ src/Providers/AuthProvider.jsx
@@ -51,7 +51,6 @@ const AuthProvider = ({children}) => {
             }
             else{
                 localStorage.removeItem('access-token');
-                setLoading(false);
             }
         })
         return () => {
